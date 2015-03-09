fa2f6f18bf53a69ac894d2f128e24f644a987248
jorseph <jorseph0418@gmail.com>
2015-03-09 15:12:02 +0800

    Commit template for STM32F429I
    You could build any project only include main function for STM32F429I
    and compile by Keil C

---
+++ STM32F429I_Original/Image/Output/system_stm32f4xx.d
@@ -0,0 +1,8 @@
+..\image\output\system_stm32f4xx.o: Sys\system_stm32f4xx.c
+..\image\output\system_stm32f4xx.o: C:\Keil\ARM\Inc\ST\STM32F4xx\stm32f4xx.h
+..\image\output\system_stm32f4xx.o: C:\Keil\ARM\CMSIS\Include\core_cm4.h
+..\image\output\system_stm32f4xx.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+..\image\output\system_stm32f4xx.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+..\image\output\system_stm32f4xx.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+..\image\output\system_stm32f4xx.o: C:\Keil\ARM\CMSIS\Include\core_cm4_simd.h
+..\image\output\system_stm32f4xx.o: C:\Keil\ARM\Inc\ST\STM32F4xx\system_stm32f4xx.h
