79308773eedab76679f90779f22afe66d9db20b1
jorseph <jorseph0418@gmail.com>
2015-03-10 23:36:38 +0800

    Build RTOS 8.2.0 based on STM32F429I

--- STM32F429I_Original/Image/Output/main.d
+++
@@ -1,8 +0,0 @@
-..\image\output\main.o: Usr\main.c
-..\image\output\main.o: C:\Keil\ARM\Inc\ST\STM32F4xx\stm32f4xx.h
-..\image\output\main.o: C:\Keil\ARM\CMSIS\Include\core_cm4.h
-..\image\output\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-..\image\output\main.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
-..\image\output\main.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
-..\image\output\main.o: C:\Keil\ARM\CMSIS\Include\core_cm4_simd.h
-..\image\output\main.o: C:\Keil\ARM\Inc\ST\STM32F4xx\system_stm32f4xx.h
--- STM32F429I_Original/Image/Output/startup_stm32f429_439xx.d
+++
@@ -1,1 +0,0 @@
-..\image\output\startup_stm32f429_439xx.o: Sys\startup_stm32f429_439xx.s
